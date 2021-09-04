d28213644d3ae26da67cff3031033a81ef8d618e
Duy Anh Nguyễn <55968905+nguynduyanh169@users.noreply.github.com>
2021-09-04 20:47:13 +0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Nguyễn Duy Anh on 04/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
